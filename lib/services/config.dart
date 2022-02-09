f0eaaafc9b209e1f5fa7e283f67f9c19a73b2e4f
fcraveiro <dextruz@hotmail.com>
2022-02-09 01:09:59 -0300

    Cropper

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,3 +1,2 @@
-const supabaseUrl = 'https://xrhyhsbetlnzksauwrvi.supabase.co';
-const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA3NTI4MywiZXhwIjoxOTUzNjUxMjgzfQ.gsz31qxeQ_h6R_93rthZwynvz1i8jNrXLz30JaFprqA';
+const supabaseUrl = '';
+const supabaseKey = '';
